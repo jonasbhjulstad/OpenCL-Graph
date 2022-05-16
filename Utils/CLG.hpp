676f90e828cb0d89095f3c73d9ca2ee2ba371ef4
Jonas <jonasbhjulstad@gmail.com>
2022-05-16 09:23:17 +0200

    Cleaned out generators

--- Utils/CLG.hpp
+++ Utils/CLG.hpp
@@ -2,7 +2,6 @@
 #define CLG_HPP
 #include "CLG_Debug.hpp"
 #include "CLG_FilePaths.hpp"
-#include "CLG_Kernel_Generators.hpp"
 #include "CLG_Util.hpp"
 #include "CLG_DefaultInstance.hpp"
 #include "CLG_directives.hpp"
