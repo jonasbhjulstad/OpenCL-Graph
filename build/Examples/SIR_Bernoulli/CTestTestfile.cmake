8a480623f1e85473749170c66582caccd4fcc2da
Jonas <jonasbhjulstad@gmail.com>
2022-06-01 10:43:10 +0200

    RNG fix Bernoulli

--- build/Examples/SIR_Bernoulli/CTestTestfile.cmake
+++ build/Examples/SIR_Bernoulli/CTestTestfile.cmake
@@ -1,6 +1,6 @@
 # CMake generated Testfile for 
-# Source directory: C:/Users/jonas/Documents/OpenCL-Graph/Examples/SIR_Bernoulli
-# Build directory: C:/Users/jonas/Documents/OpenCL-Graph/build/Examples/SIR_Bernoulli
+# Source directory: /home/deb/Documents/OpenCL-Graph/Examples/SIR_Bernoulli
+# Build directory: /home/deb/Documents/OpenCL-Graph/build/Examples/SIR_Bernoulli
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
